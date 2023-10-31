74a5fb34a6b111308103060b9999166eb96b7074
DemianVGLl2 <0253139@up.edu.mx>
2023-10-31 10:46:21 -0600

    Update: 31/10/2023

--- Portfolio/09-ExpressMongoDB/server.js
+++ Portfolio/09-ExpressMongoDB/server.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyparser = require('body-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
@@ -13,15 +15,15 @@ app.set("view engine", "ejs");
 const user = process.env.USER_ID;
 const password = process.env.USER_PASS;
 
-const mongoUrl = `mongodb+srv://${user}:${password}@demian.zfaifux.mongodb.net/?retryWrites=true&w=majority`;
+const mongoUrl = `mongodb+srv://${user}:${password}@demian.zfaifux.mongodb.net/f1?retryWrites=true&w=majority`;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Definition of a schema
 const teamSchema = new mongoose.Schema({
   id: Number,
+  code: String,
   name: String,
-  value: String,
-  nationality: String,
+  country: String,
   url: String,
 });
 teamSchema.set("strictQuery", true);
@@ -67,23 +69,82 @@ let teamsRaw = [
 
 let teams = [];
 
+let drivers = [];
+
 app.use("/", async (req, res, next) => {
     if (teams.length == 0) {
         var team = await Team.find({}).exec();
         if (!Array.isArray(team) || team.length == 0) {
-            //insert elements
+          //Option 1
+          await Team.insertMany(teamsRaw).then(() => {
+            console.log("Succes!");
+          }
+          ).catch((error) => {
+            console.error(error);
+          });
 
+          //Option 2
+          await Team.find({}, (err, docs) => {
+            if (err) {
+              console.error(err);
+            }
+            else {
+              console.log("Teams loaded");
+              teams=docs;
+            }
+          });
         }
         else {
             teams = team;
         }
     }
+    next();
 });
 
 app.get("/", (req, res) => {
   // res.sendFile(__dirname + "/public/html/index.html");
-  res.render("index", { countries, teamsRaw: teams });
+  res.render("index", { countries, teams, drivers });
 });
+
+app.post("/driver", async(req, res, next) => {
+  var team = await Team.findOne({code: {$eq: req.body.team}}).exec();
+  //Method 1: Using direct mode
+  /*var driver = {
+    num: req.body.name,
+    code: req.body.code,
+    forename: req.body.forename,
+    surname: req.body.surname,
+    dob: req.body.dob,
+    url: req.body.url,
+    nationality: req.body.nation,
+    team: team,
+  };
+
+  await Driver.insertOne(driver).then(() => {
+    console.log("Driver recorded");
+  })
+  .catch((err) => {
+    console.error(err);
+  })*/
+
+  //Method 2
+  var driver = new Driver({
+    num: req.body.num,
+    code: req.body.code,
+    forename: req.body.name,
+    surname: req.body.lname,
+    dob: req.body.dob,
+    nationality: req.body.nation,
+    url: req.body.url,
+    team: team,
+  });
+  
+  driver.save();
+  drivers.push(driver);
+  res.redirect('/');
+});
+
+
 
 app.listen(3000, (err) => {
   console.log("Listening on port 3000");
